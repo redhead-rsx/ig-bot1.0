d92b75855f2731a69ac5b45201b24117c821f329
redhead-rsx <gabrieldiassoares831@gmail.com>
2025-08-21 21:38:56 -0300

    Remove liker feature

--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
-chrome.storage.sync.get(['minDelay', 'maxDelay', 'limite', 'curtirFoto'], (data) => {
+chrome.storage.sync.get(['minDelay', 'maxDelay', 'limite'], (data) => {
     document.getElementById('quantidade').value = data.limite || 10;
-    document.getElementById('curtirFoto').checked = data.curtirFoto !== undefined ? data.curtirFoto : true;
     document.getElementById('minDelay').value = data.minDelay || 120;
     document.getElementById('maxDelay').value = data.maxDelay || 180;
 });
@@ -13,13 +12,12 @@ function sendMessageToActiveTab(message) {
 
 document.getElementById('startBtn').addEventListener('click', () => {
     const limite = parseInt(document.getElementById('quantidade').value) || 10;
-    const curtirFoto = document.getElementById('curtirFoto').checked;
     const minDelay = parseInt(document.getElementById('minDelay').value) || 120;
     const maxDelay = parseInt(document.getElementById('maxDelay').value) || 180;
 
-    chrome.storage.sync.set({ minDelay, maxDelay, limite, curtirFoto });
+    chrome.storage.sync.set({ minDelay, maxDelay, limite });
 
-    sendMessageToActiveTab({ action: 'start', limite, curtirFoto, minDelay, maxDelay });
+    sendMessageToActiveTab({ action: 'start', limite, minDelay, maxDelay });
 });
 
 document.getElementById('stopBtn').addEventListener('click', () => {
